d3dbeaa85ad65af0044a372d2eaf52690dc6573e
Kamil <kamil.filipczyk.kontakt@gmail.com>
2024-11-06 11:13:43 +0100

    Add project files.

---
+++ QuickNotes/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace QuickNotes
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ QuickNotes/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace QuickNotes
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
