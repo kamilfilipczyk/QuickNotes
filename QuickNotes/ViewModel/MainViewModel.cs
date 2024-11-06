de7768bfb50890c43f9820dedb47579e50a4744e
Kamil <kamil.filipczyk.kontakt@gmail.com>
2024-11-06 12:39:57 +0100

    Add MainViewModel and bind NewNote property to Entry

---
+++ QuickNotes/ViewModel/MainViewModel.cs
@@ -0,0 +1,10 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace QuickNotes.ViewModel
+{
+    public partial class MainViewModel: ObservableObject
+    {
+        [ObservableProperty]
+        string newNote;
+    }
+}
